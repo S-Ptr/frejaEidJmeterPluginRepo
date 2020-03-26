0a80e6c6fac3e6d4b20b54baa0acc3d0d64ee7ef
risticivan12 <risticivan@yahoo.com>
2020-03-26 16:46:50 +0100

    fix imports

--- src/main/java/com/internship/frejaeidjmeterplugin/jmeter/sampler/GeneralSampler.java
+++ src/main/java/com/internship/frejaeidjmeterplugin/jmeter/sampler/GeneralSampler.java
@@ -1,15 +1,6 @@
 package com.internship.frejaeidjmeterplugin.jmeter.sampler;
 
-import com.internship.frejaeidjmeterplugin.jmeter.frejaRequests.AuthenticationService;
-import com.internship.frejaeidjmeterplugin.jmeter.frejaRequests.SignService;
-import com.verisec.frejaeid.client.beans.authentication.get.AuthenticationResult;
-import com.verisec.frejaeid.client.beans.sign.get.SignResult;
-import com.verisec.frejaeid.client.enums.MinRegistrationLevel;
 import com.verisec.frejaeid.client.exceptions.FrejaEidClientInternalException;
-import com.verisec.frejaeid.client.exceptions.FrejaEidClientPollingException;
-import com.verisec.frejaeid.client.exceptions.FrejaEidException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.apache.jmeter.samplers.AbstractSampler;
 import org.apache.jmeter.samplers.Entry;
 import org.apache.jmeter.samplers.SampleResult;
